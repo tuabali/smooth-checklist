a72f3bce57fb5d4d0072a1cd8a0e3fc073b5a6b9
Turab_Ali <appsapiconsole@gmail.com>
2026-01-12 16:51:17 +0500

    changes by turabi 2026-01-12-0450

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+//    alias(libs.kotlin.metadata.jvm)
+
+}
+
+android {
+    namespace = "com.example.smoothchecklist"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.smoothchecklist"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildFeatures{
+        viewBinding = true
+        buildConfig = true
+    }
+
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+}
--- app/src/main/java/com/example/smoothchecklist/ui/MainActivity.kt
+++ app/src/main/java/com/example/smoothchecklist/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.smoothchecklist.data.ChecklistItem
 import com.example.smoothchecklist.databinding.ActivityMainBinding
 import com.example.smoothchecklist.ui.checklist.ChecklistAdapter
 import com.example.smoothchecklist.ui.checklist.ChecklistViewModel
@@ -24,14 +25,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-        binding.toolbar.setOnMenuItemClickListener { menuItem ->
-            if (menuItem.itemId == com.example.smoothchecklist.R.id.action_done) {
-                viewModel.addItem()
-                true
-            } else {
-                false
-            }
+//        setSupportActionBar(binding.toolbar)
+//        binding.toolbar.setOnMenuItemClickListener { menuItem ->
+//            if (menuItem.itemId == com.example.smoothchecklist.R.id.action_done) {
+//                viewModel.addItem()
+//                true
+//            } else {
+//                false
+//            }
+//        }
+
+        binding.ivTick.setOnClickListener {
+            viewModel.addItem()
         }
 
         val adapter = ChecklistAdapter(
@@ -46,7 +51,8 @@ class MainActivity : AppCompatActivity() {
         binding.checklistRecycler.layoutManager = LinearLayoutManager(this)
         binding.checklistRecycler.adapter = adapter
         binding.checklistRecycler.setHasFixedSize(true)
-
+        val list = arrayListOf<ChecklistItem>(ChecklistItem(1L, false, "test one"))
+        adapter.submitList(list)
         viewModel.checklistItems.observe(this) { items ->
             adapter.submitList(items)
         }
@@ -56,5 +62,6 @@ class MainActivity : AppCompatActivity() {
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
